e40d59e3330936ab06a9250fff64f5c78c115be5
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-21 16:25:53 -0300

    feat: stylize home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,42 @@
-"use client";
+"use client"
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+
+const Container = styled('div')({
+  
+})
+
+const Heading = styled('h1')({
+  color: 'black',
+  fontSize: '65px',
+  fontWeight: "600",
+  textTransform: "capitalize",
+  lineHeight: "1.1em",
+  letterSpacing: "-0.65px",
+  width: "100%",
+  maxWidth: "250px",
+});
+
+const Text = styled('p')({
+  fontSize: "14px",
+  letterSpacing: "-0.32px",
+  lineHeight: "1.5em",
+  width: "100%",
+  maxWidth: "443px",
+  paddingBottom: "15px",
+})
 
 export default function Home() {
   return (
-    <div>
-      <h1>Crie um deslocamento</h1>
-      <p>
+    <Container>
+      <Heading>Crie um deslocamento</Heading>
+      <Text>
         Deslocamento é uma solução que permite gerenciar eficientemente
         deslocamentos. Ele oferece recursos para clientes, condutores e
         veículos, permitindo a criação, edição e exclusão de informações.
-      </p>
+      </Text>
 
-      <button>Entrar</button>
-    </div>
+      <Button size="large" variant="contained" href="/about">Link</Button>
+    </Container>
   );
 }
