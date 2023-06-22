9969854c1a3fce4596fbab1b9097fccd441bb287
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-22 00:07:49 -0300

    feat: create client card and receive properties

---
+++ src/components/Cards/CardClient.tsx
@@ -0,0 +1,51 @@
+import { CardLayout } from "./CardLayout";
+import car from "../../assets/car.png";
+import { Result, Text } from "./style";
+
+interface IClient {
+  numeroDocumento: string;
+  tipoDocumento: string;
+  nome: string;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+}
+
+interface IClientComponent {
+  client: IClient;
+}
+
+const CardClient = ({ client }: IClientComponent) => {
+  return (
+    <CardLayout alt="Car" src={car}>
+      <Text>
+        Número do Documento: <Result>{client.numeroDocumento}</Result>
+      </Text>
+      <Text>
+        Tipo do Documento: <Result>{client.tipoDocumento}</Result>
+      </Text>
+      <Text>
+        Nome: <Result>{client.nome}</Result>
+      </Text>
+      <Text>
+        Logradouro: <Result>{client.logradouro}</Result>
+      </Text>
+      <Text>
+        Número: <Result>{client.numero}</Result>
+      </Text>
+      <Text>
+        Bairro: <Result>{client.bairro}</Result>
+      </Text>
+      <Text>
+        Cidade: <Result>{client.cidade}</Result>
+      </Text>
+      <Text>
+        UF: <Result>{client.uf}</Result>
+      </Text>
+    </CardLayout>
+  );
+};
+
+export { CardClient };
