88e0a29f802cdfe74720ab2ad37c2a6fe5cceb81
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-06-22 00:29:10 -0300

    feat: create displacement card and receive properties

---
+++ src/components/Cards/CardDisplacement.tsx
@@ -0,0 +1,52 @@
+import { CardLayout } from "./CardLayout";
+import user from "../../assets/user.png";
+import { Result, Text } from "./style";
+
+interface IDisplacement {
+  kmInicial: number;
+  inicioDeslocamento: string;
+  checkList: string;
+  motivo: string;
+  observacao: string;
+  idCondutor: number;
+  idVeiculo: number;
+  idCliente: number;
+}
+
+interface IDisplacementComponent {
+  displacement: IDisplacement;
+}
+
+const CardDisplacement = ({ displacement }: IDisplacementComponent) => {
+  return (
+    <CardLayout alt="User" src={user}>
+      <Text>
+        KM Inicial: <Result>{displacement.kmInicial}</Result>
+      </Text>
+      <Text>
+        Inicio do Deslocamento:{" "}
+        <Result>{displacement.inicioDeslocamento}</Result>
+      </Text>
+      <Text>
+        Check List: <Result>{displacement.checkList}</Result>
+      </Text>
+      <Text>
+        Motivo: <Result>{displacement.motivo}</Result>
+      </Text>
+      <Text>
+        Observação: <Result>{displacement.observacao}</Result>
+      </Text>
+      <Text>
+        ID Condutor: <Result>{displacement.idCondutor}</Result>
+      </Text>
+      <Text>
+        ID Veículo: <Result>{displacement.idVeiculo}</Result>
+      </Text>
+      <Text>
+        ID Cliente: <Result>{displacement.idCliente}</Result>
+      </Text>
+    </CardLayout>
+  );
+};
+
+export { CardDisplacement };
